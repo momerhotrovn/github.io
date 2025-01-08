99bd16f37b4cee978d6dbaf101327d1ecdccab65
momerhotrovn <momer.hotro@gmail.com>
2025-01-09 02:33:05 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Form Submission
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("Yêu cầu của bạn đã được gửi. Chúng tôi sẽ liên hệ sớm nhất.");
+});
